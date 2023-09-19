30d99ce53f0f25acd066bf4282ca28716de04267
iblanco-s <113044030+iblanco-s@users.noreply.github.com>
2023-09-19 11:59:04 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,4 +1,4 @@
-
+wget -q -O - https://nightly.odoo.com/odoo.key | sudo gpg --dearmor -o /usr/share/keyrings/odoo-archive-keyring.gpg
 # Añade el repositorio de Odoo al archivo de fuentes
 echo 'deb [signed-by=/usr/share/keyrings/odoo-archive-keyring.gpg] https://nightly.odoo.com/16.0/nightly/deb/ ./' | sudo tee /etc/apt/sources.list.d/odoo.list
 
